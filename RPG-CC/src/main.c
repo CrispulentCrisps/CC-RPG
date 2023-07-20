d0ce1bc6f2db7a36c0c0ce59039c1d1a83e57760
CrispulentCrisps <70386744+CrispulentCrisps@users.noreply.github.com>
2023-07-20 02:24:47 +0100

    Start

---
+++ RPG-CC/src/main.c
@@ -0,0 +1,15 @@
+/**
+ * Hello World Example
+ * Created With Genesis-Code extension for Visual Studio Code
+ * Use "Genesis Code: Compile" command to compile this program.
+ **/
+#include <genesis.h>
+
+int main()
+{
+    while(1)
+    {
+        SYS_doVBlankProcess();
+    }
+    return (0);
+}
